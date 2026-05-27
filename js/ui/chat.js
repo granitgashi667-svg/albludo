b8a9a15525844912f3ab0fc8454b2752d5c250f3
granitgashi667-svg <granitgashi667@gmail.com>
2026-05-27 03:20:10 +0200

    Update chat.js

--- js/ui/chat.js
+++ js/ui/chat.js
@@ -1,9 +1,11 @@
-export function sendChatMessage(playerName) {
+import { playerData } from '../players/playerData.js';
+
+export function sendChatMessage() {
     const input = document.getElementById("chatInput");
     if (!input.value.trim()) return;
     const msg = input.value;
     const chatDiv = document.getElementById("chatMessages");
-    chatDiv.innerHTML += `<div><i class="fas fa-comment"></i> ${playerName}: ${msg}</div>`;
+    chatDiv.innerHTML += `<div><i class="fas fa-comment"></i> ${playerData.name}: ${msg}</div>`;
     input.value = "";
     chatDiv.scrollTop = chatDiv.scrollHeight;
 }
